a43791a010c95fe7d28981f2870d1bd201cd195e
kauanmaldonado <kauan.maldonado16@gmail.com>
2021-07-14 19:24:42 -0300

    Initial commit

---
+++ src/OlaMundo.java
@@ -0,0 +1,11 @@
+
+import javax.swing.JOptionPane;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        //System.out.println("Olá mundo");
+
+        JOptionPane.showMessageDialog(null,"OLÁ KAUAN");
+    }
+
+}
